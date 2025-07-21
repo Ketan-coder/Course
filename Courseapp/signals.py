07d2394f55c631cff964a23a37723946807a0d9c
ketanv288 <ketanv288@gmail.com>
2025-07-21 12:59:40 +0000

    Working On Quiz Submission Logic

--- Courseapp/signals.py
+++ Courseapp/signals.py
@@ -1,10 +1,24 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Course, Quiz
+from .models import Course, Quiz, QuizSubmission
 
 
 @receiver(post_save, sender=Quiz)
-def reward_quiz_completion(sender, instance, created, **kwargs):
+def quiz_completion(sender, instance, created, **kwargs):
     if not created:
-        pass
+        question_count = len(instance.questions or {})
+        print(question_count)
+        completed_count = sum(1 for qid, qdata in (instance.questions or {}).items() if qdata["is_completed"] == True)
+        print(completed_count)
+        total_earned_points = sum(qdata["score_on_completion"] for qid, qdata in (instance.questions or {}).items() if qdata["is_completed"] == True)
+        print(total_earned_points)
+        if question_count != 0 and question_count == completed_count:
+            QuizSubmission.objects.create(
+                quiz_id=instance.id,
+                user=request.user.profile,
+                score=total_earned_points,
+                total=total_earned_points,
+                passed=True,
+                answers=instance.questions
+            )
